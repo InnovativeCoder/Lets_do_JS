1212584ab471d2dcbdf26b5cdda1999ee7569a41
Jasneet Singh Sawhney <aashitsingh2010@gmail.com>
2018-08-20 22:07:24 +0530

    Learnt basic Syntax

---
+++ LCO/01Basics/Variables.js
@@ -0,0 +1,13 @@
+let name = 'Innovative'
+let score = '100'
+let num = 10
+
+console.log(name)
+
+let firstname = 'John'
+let lastname = 'Doe'
+
+let concat = firstname +' ' + lastname
+
+console.log(concat)
+console.log(firstname +' ' + lastname)
