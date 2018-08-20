6a929b7fc442f94b59ca92d29b12ab0e9722bd6a
Jasneet Singh Sawhney <aashitsingh2010@gmail.com>
2018-08-20 22:47:24 +0530

    basics done :smile:

---
+++ LCO/01Basics/celsius.js
@@ -0,0 +1,24 @@
+var score = 100
+
+score = 130
+
+const bonus = 50
+
+var finalscore = (score + bonus) * 1.8
+
+console.log(finalscore);
+
+var complex = (4 + 4 ) * 5
+
+console.log(complex)
+
+
+// Temperature from Far. to Cel.
+
+let tempinfar = 50
+
+//do calculation
+
+let celsius = (tempinfar - 32) * 5/9
+
+console.log(celsius)
---
+++ LCO/01Basics/nullandbool.js
@@ -0,0 +1,15 @@
+var temp = 0
+
+console.log("current temperature is : " + temp)
+
+//Grade System 
+
+let grade = true
+
+console.log(grade)
+
+let actualmarks = 7
+let mygrade= (actualmarks< 10)
+
+
+console.log(mygrade)
