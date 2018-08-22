a70f6d21ffb4d521d71258a795238d404c0bd060
Jasneet Singh Sawhney <aashitsingh2010@gmail.com>
2018-08-22 18:40:57 +0530

    Updated some more :smile:

---
+++ LCO/02Basics/marvels.js
@@ -0,0 +1,31 @@
+// datatype - Array
+
+let one = 'one'
+let two = 'two'
+
+
+const superheroes =  ['Iron Man','Spider Man',1, true]
+
+console.log(superheroes)
+
+console.log(`We have ${superheroes.length} super heroes`);
+
+const number = ['One', 'Two', 'Three', 'Four','Five','Six']
+number[1] = 'SOMETHING'
+
+console.log(number)
+
+//start
+
+number.shift()
+
+console.log(number)
+
+number.unshift('Anything')
+
+console.log(number)
+
+//end
+
+console.log(number.pop()) 
+console.log(number)
