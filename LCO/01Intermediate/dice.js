18da14ff6d8037a94038ceb540f694a628d6d641
Jasneet Singh Sawhney <aashitsingh2010@gmail.com>
2018-08-27 21:30:51 +0530

    Intermediate level achieved

---
+++ LCO/01Intermediate/dice.js
@@ -0,0 +1,19 @@
+let numOne = 22
+let numTwo = 7
+
+result = numOne/numTwo
+
+console.log(22/7)
+console.log(result.toFixed(2))
+console.log(Math.floor(result))
+
+console.log(Math.random())
+
+
+//dice roller
+ let lower = 20
+ let upper = 25
+
+ let myRandom = Math.floor(Math.random() * (upper - lower + 1))
+
+ console.log(myRandom)
